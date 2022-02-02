3d390064a499d12e2ef17681b5f0b45319384aae
Rod Morgado <rodrigo@zooft.company>
2022-02-01 18:33:26 -0800

    getting matches and their leagues name with goals larger than 10

--- working-with-ctes.sql
+++ working-with-ctes.sql
@@ -13,3 +13,20 @@ FROM league AS l
 -- Join the CTE to the league table
 LEFT JOIN match_list ON l.id = match_list.country_id
 GROUP BY l.name;
+
+-- Set up your CTE
+WITH match_list AS (
+  -- Select the league, date, home, and away goals
+    SELECT 
+  		l.name AS league, 
+     	m.date, 
+  		m.home_goal, 
+  		m.away_goal,
+       (m.home_goal + m.away_goal) AS total_goals
+    FROM match AS m
+    LEFT JOIN league as l ON m.country_id = l.id)
+-- Select the league, date, home, and away goals from the CTE
+SELECT league, date, home_goal, away_goal
+FROM match_list
+-- Filter by total goals
+WHERE total_goals >= 10;
