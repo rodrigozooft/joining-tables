dff6d909f1835ccf1f176a89181e0b4e1187f69d
Rod Morgado <rodrigo@zooft.company>
2022-02-01 18:39:27 -0800

    seleting the average goals by matches season and for the august month

--- working-with-ctes.sql
+++ working-with-ctes.sql
@@ -30,3 +30,23 @@ SELECT league, date, home_goal, away_goal
 FROM match_list
 -- Filter by total goals
 WHERE total_goals >= 10;
+
+-- Set up your CTE
+WITH match_list AS (
+    SELECT 
+  		country_id,
+  	   (home_goal + away_goal) AS goals
+    FROM match
+  	-- Create a list of match IDs to filter data in the CTE
+    WHERE id IN (
+       SELECT id
+       FROM match
+       WHERE season = '2013/2014' AND EXTRACT(MONTH FROM date) = '08'))
+-- Select the league name and average of goals in the CTE
+SELECT 
+	l.name,
+    AVG(goals)
+FROM league AS l
+-- Join the CTE onto the league table
+LEFT JOIN match_list ON l.id = match_list.country_id
+GROUP BY l.name;
