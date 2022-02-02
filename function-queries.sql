62e5d88cbde9aa01feb113896c4a597b1133ea27
Rod Morgado <rodrigo@zooft.company>
2022-02-01 21:36:31 -0800

    working with partition by season to get the average of matches

--- function-queries.sql
+++ function-queries.sql
@@ -37,3 +37,20 @@ WHERE m.season = '2011/2012'
 GROUP BY l.name
 -- Order the query by the rank you created
 ORDER BY league_rank;
+
+SELECT
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+		 ELSE 'away' END AS warsaw_location,
+    -- Calculate the average goals scored partitioned by season
+    AVG(home_goal) OVER(PARTITION BY season) AS season_homeavg,
+    AVG(away_goal) OVER(PARTITION BY season) AS season_awayavg
+FROM match
+-- Filter the data set for Legia Warszawa matches only
+WHERE 
+	hometeam_id = 8673 
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
